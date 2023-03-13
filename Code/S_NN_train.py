ea8c751591594700d86d739fb997f8fc08094cb8
Samaksh Chandra <64631239+codeasy586@users.noreply.github.com>
2023-03-13 12:29:52 +0000

    Update S_NN_train.py

--- Code/S_NN_train.py
+++ Code/S_NN_train.py
@@ -131,23 +131,23 @@ def NN_train(pathdir, filename, epochs=1000, lrs=1e-2, N_red_lr=4, pretrained_pa
                         fct = data[0].float()
                         prd = data[1].float()
                     
-                    loss = rmse_loss(model_feynman(fct),prd).cpu()
-                    loss.cpu().backward()
+                    loss = rmse_loss(model_feynman(fct),prd).detach.cpu().numpy()
+                    loss.detach().cpu().numpy().backward()
                     optimizer_feynman.step()
 
                 # Early stopping
                 if epoch%20==0 and epoch>0:
-                    if check_es_loss < loss:
+                    if check_es_loss < loss.detach().cpu().numpy():
                         break
                     else:
                         torch.save(model_feynman.state_dict(), "results/NN_trained_models/models/" + filename + ".h5")
-                        check_es_loss = loss
+                        check_es_loss = loss.detach().cpu().numpy()
                 if epoch==0:
-                    if check_es_loss < loss:
+                    if check_es_loss < loss.detach().cpu().numpy():
                         torch.save(model_feynman.state_dict(), "results/NN_trained_models/models/" + filename + ".h5")
-                        check_es_loss = loss
+                        check_es_loss = loss.detach().cpu().numpy()
         
-            print(loss)
+            print(loss.detach().cpu().numpy()
             lrs = lrs/10
 
         return 1
